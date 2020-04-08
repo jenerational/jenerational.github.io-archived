6def99e03515a26be1abb838830185df35180a1b
jenerational <jw4043@nyu.edu>
2020-04-08 11:50:35 -0400

    colors update
    fixed program, added color pallete

--- programming/p5/colors/sketch.js
+++ programming/p5/colors/sketch.js
@@ -6,9 +6,9 @@ var bgCol = 200;
 var mClick = false; 
 var colName; 
 // color vars
-var red;
-var green;
-var blue; 
+var xRed;
+var xGreen;
+var xBlue; 
 // darker colors
 var dRed;
 var dGreen;
@@ -30,7 +30,7 @@ function setup() {
   // load in data
   for(var i = 0; i < hexData.colors.length; i++){
     colArr.push(new ColDot(hexData.colors[i].color, hexData.colors[i].hex)); 
-    console.log("push"); 
+    //console.log("push"); 
     caSz++; 
   }
 }
@@ -40,7 +40,6 @@ function draw() {
   for (var i = 0; i < caSz; i++) {
     colArr[i].display(); 
   }
-  down = false; 
 }
 
 function keyPressed() {
@@ -57,45 +56,43 @@ function keyPressed() {
   }
 }
 
-function mousePressed() {
-  mClick = true; 
-  colName = false;
-}
-
 //  CLASSES
 function ColDot(name, hex) {
   this.x = random(windowWidth);
   this.y = random(windowHeight); 
   this.name = name; 
   this.color = color(hex); 
-  console.log(this.color); 
-  this.dia = 20; 
+  //console.log(this.color); 
+  this.dia = 11; 
   this.display = function() {
     fill(this.color); 
     ellipse (this.x, this.y, this.dia, this.dia); 
     if (mouseX >= this.x-this.dia/2 && mouseX < this.x + this.dia/2) {
       if (mouseY >= this.y-this.dia/2 && mouseY < this.y + this.dia/2) {
         background(250, 250, 250, .2);
-        textSize(20);
-        if (colName=true){
-          text(this.name, mouseX+10, mouseY+10); 
-        }
-        if(mClick) {
-          red = unhex(this.color.substring(0, 2));
-          green = unhex(this.color.substring(2, 4));
-          blue = unhex(str.substring(4, 6));
-          console.log(red+", "+green+", "+blue)
-          dRed = (red-50>0) ? red-50:0; 
-          dBlue = (blue-50>0) ? blue-50:0; 
-          dGreen = (green-50>0) ? green-50:0; 
-          lRed = (red+50<255) ? red+50:255; 
-          lBlue = (blue+50<255) ? blue+50:255;
-          lGreen = (green+50<255) ? green+50:255;
+        textSize(15); 
+        text(this.name, mouseX+10, mouseY+10); 
+          //BAHAHAHAHAHAHA
+          //thRed =str(this.color);
+          //console.log(thRed.substring(0,2)); 
+          //xred = unhex(str(this.color).substring(0, 2));
+          //green = unhex(str(this.color).substring(2, 4));
+          //blue = unhex(str(this.color).substring(4, 6));
+          //console.log(red+", "+green+", "+blue)
+          xRed = red(this.color);
+          xGreen = green(this.color);
+          xBlue = blue(this.color); 
+          console.log(xRed+", "+xGreen+", "+xBlue); 
+          dRed = (xRed-50>0) ? xRed-50:0; 
+          dBlue = (xBlue-50>0) ? xBlue-50:0; 
+          dGreen = (xGreen-50>0) ? xGreen-50:0; 
+          lRed = (xRed+50<255) ? xRed+50:255; 
+          lBlue = (xBlue+50<255) ? xBlue+50:255;
+          lGreen = (xGreen+50<255) ? xGreen+50:255;
           fill(lRed,lGreen,lBlue); 
           ellipse(this.x+this.dia, this.y, this.dia, this.dia); 
           fill(dRed,dGreen,dBlue); 
           ellipse(this.x-this.dia, this.y, this.dia, this.dia);
-        }
       }
     }
   }
